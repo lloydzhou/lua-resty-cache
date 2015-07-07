aaee23b516e1aba2c39b68f6e3f9809d3f734d78
lloydzhou <lloydzhou@qq.com>
2015-07-07 15:58:12 +0800

    upload code to github

---
+++ lib/resty/cache.lua
@@ -0,0 +1,96 @@
+local parser = require "redis.parser"
+local json = require "cjson"
+local lock = require "resty.lock"
+
+local _M = { _VERSION = '0.1'  }
+local mt = {__index = _M}
+local loglevel = ngx.NOTICE
+local find = function(value, list)
+    for _, v in pairs(list) do
+        if v == value then return true end
+    end
+    return false
+end
+local query = function(cache, queries)
+    local raw = {}
+    for i, q in ipairs(queries) do
+        table.insert(raw, parser.build_query(q))
+    end
+    local response = ngx.location.capture(cache, {method=ngx.HTTP_POST, args={n=#raw}, body=table.concat(raw, "")})
+    if response.status ~= 200 or not response.body then
+        ngx.log(loglevel, "[LUA] failed to query redis data")
+    end
+    return response
+end
+local out = function(status, headers, body, cache_header, cache_status)
+    for n, v in pairs(headers or {}) do
+        ngx.header[n] = v
+    end
+    if cache_header then
+        ngx.header[cache_header] = cache_status or ""
+    end
+    ngx.status = tonumber(status)
+    ngx.print(body)
+    ngx.eof()
+end
+local cache = function(self, key, output, l)
+    local response = ngx.location.capture(self.fallback .. ngx.var.request_uri)
+    if output then
+        out(response.status, response.header, response.body, self.header, "STORE")
+    end
+    if find(response.status, self.status) then
+        local exp_header, cache_control = response.header['Expire'], response.header['Cache-Control']
+        local t = exp_header and ngx.parse_http_time(exp_header) - ngx.now()
+        if cache_control then
+            local _, _, _, age = string.find(cache_control, "(.*):(.*)")
+            t = age and tonumber(age)
+        end
+        query(self.cache, {
+            {"MULTI"},
+            {"HMSET", key, "status", response.status, "headers", json.encode(response.header), "body", response.body},
+            {"EXPIRE", key, (t or self.age) + self.stale},
+            {"EXEC"}})
+        if l then l:unlock() end
+        ngx.log(loglevel, "[LUA] cache store on cache key: ", key)
+    end
+end
+function get(self, key, l)
+    local response = query(self.cache, {
+        {"TTL", key},
+        {"HGET", key, "status"},
+        {"HGET", key, "headers"},
+        {"HGET", key, "body"}})
+    local res = parser.parse_replies(response.body, 4)
+    local ttl = tonumber(res[1][1])
+    if not (res == nil) and (#res == 4) and ttl > -2 then
+        ngx.log(loglevel, "[LUA] cache hit on cache key: ", key, ", TTL: ", ttl)
+        out(res[2][1], json.decode(res[3][1]), res[4][1], self.header, ttl <= self.stale and "STALE" or "HIT")
+        if l then l:unlock() end
+        if ttl < self.stale then
+            ngx.log(loglevel, "[LUA] cache stale on cache key: ", key, ", TTL: ", ttl)
+            l = lock:new(self.lockname, {timeout=0.01})
+            if l:lock(key) then cache(self, key, false, l) end
+        end
+    else
+        if l then cache(self, key, true, l) else
+            l = lock:new(self.lockname, {timeout=3})
+            if l:lock(key) then get(self, key, l) end
+        end
+    end
+end
+---------------------------------
+function _M.new(_, lockname, cache, fallback, status, methods, age, stale, header)
+    return setmetatable({
+        lockname=lockname,
+        cache=cache, fallback=fallback,
+        status=status or {ngx.HTTP_OK}, methods=methods or {"GET", "HEAD"},
+        age=age or 120, stale=stale or 100, header=header}, mt)
+end
+function _M.run(self)
+    if find(ngx.var.request_method, self.methods) then
+        get(self, ngx.md5(ngx.var.request_uri), nil)
+    end
+end
+
+return _M
+
---
+++ t/sanity.t
@@ -0,0 +1,72 @@
+# vim:set ft= ts=4 sw=4 et:
+
+use Test::Nginx::Socket::Lua;
+use Cwd qw(cwd);
+
+repeat_each(2);
+
+plan tests => repeat_each() * (blocks() * 3);
+
+my $pwd = cwd();
+
+our $HttpConfig = qq{
+    lua_package_path "$pwd/lib/?.lua;;";
+    lua_package_cpath "/usr/local/openresty-debug/lualib/?.so;/usr/local/openresty/lualib/?.so;;";
+    lua_shared_dict cache_locks 100k;
+};
+
+$ENV{TEST_NGINX_RESOLVER} = '8.8.8.8';
+$ENV{TEST_NGINX_REDIS_PORT} ||= 6379;
+
+no_long_string();
+#no_diff();
+
+run_tests();
+
+__DATA__
+
+=== TEST 1: lock is subject to garbage collection
+--- http_config eval: $::HttpConfig
+--- config
+    location = /t {
+        content_by_lua '
+        local cache = require "resty.cache"
+        cache:new("cache_locks", "/redis", "/fallback", nil, nil, 10, 10, "X-Cache"):run()
+        ';
+    }
+
+    location /fallback {
+
+        rewrite ^/fallback/(.*) /$1 break;
+        proxy_set_header Host $http_host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_redirect off;
+
+        # send request to the backend
+        proxy_pass http://127.0.0.1:9999;
+        #echo hello world!!!;
+    }
+    location /redis {
+        internal;
+        default_type text/html;
+        # number of redis commands
+        set_unescape_uri $n $arg_n;
+        # we need to read body explicitly here...or $echo_request_body will evaluate to empty ("")
+        echo_read_request_body;
+        redis2_raw_queries $n "$echo_request_body\r\n";
+        redis2_connect_timeout 200ms;
+        redis2_send_timeout 200ms;
+        redis2_read_timeout 200ms;
+        redis2_pass 127.0.0.1:6379;
+        error_page 500 501 502 503 504 505 @empty_string;
+    }
+	location @empty_string {echo "";}
+--- request
+GET /t
+--- response_headers
+Content-Type: text/html; charset=UTF-8
+--- no_error_log
+[error]
+
+
